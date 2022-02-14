b82557652b997ba446190a54b1a8d522465e64eb
john smilga <johnsmilga@gmail.com>
2022-02-14 12:04:59 -0800

    domcontent loaded

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+/* 
+The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+
+window.addEventListener('DOMContentLoaded', function () {
+  // your code goes here
+});
+
+document.addEventListener....
+
+*/
+
+window.addEventListener('DOMContentLoaded', function () {
+  const heading = document.querySelector('h1');
+  console.log(heading);
+  heading.style.color = 'red';
+});
--- examples/03-dom-content-loaded.js
+++ examples/03-dom-content-loaded.js
@@ -1,4 +1,16 @@
-// show in the head element
+/* 
+The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+
 window.addEventListener('DOMContentLoaded', function () {
-  console.log('hello');
+  // your code goes here
+});
+
+document.addEventListener....
+
+*/
+
+window.addEventListener('DOMContentLoaded', function () {
+  const heading = document.querySelector('h1');
+  console.log(heading);
+  heading.style.color = 'red';
 });
