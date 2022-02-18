594c9d7bca5ec21c7c437933bcd22f5f9c984500
john smilga <johnsmilga@gmail.com>
2022-02-18 12:48:51 -0800

    class name

--- DOM/4-getElementsByTagName/app.js
+++ DOM/4-getElementsByTagName/app.js
@@ -9,22 +9,18 @@
 // NodeList - objects are collections of nodes
 // can run forEach
 // turn them into array - spread operator [...]
-// after that can use any array properties
+// after that can use any array properties and methods
 
 const headings = document.getElementsByTagName('h2');
 headings[0].style.color = 'red';
 // console.log(headings.length);
 
 const items = document.getElementsByTagName('li');
-
-// items.forEach(function(item) {
-//   console.log(item);
-// });
 items[2].style.color = 'orange';
 const betterItems = [...items];
 
 betterItems.forEach(function (item) {
-  // console.log(item);
+  console.log(item);
 });
 
 console.log(items);
---
+++ DOM/5-getElementsByClassName/app.js
@@ -0,0 +1,10 @@
+// Select the element or group of elements the we want
+// Decide the effect we want to apply to the selection
+
+// getElementsByClassName('classname');
+
+// HTMLCollection
+
+const listItems = document.getElementsByClassName('special');
+listItems[2].style.color = 'blue';
+console.log(listItems);
