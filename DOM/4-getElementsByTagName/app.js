2327450a72418e7d6c13a237834a9c5f45e739dd
john smilga <johnsmilga@gmail.com>
2022-02-18 11:56:19 -0800

    get elements by tagname added

---
+++ DOM/4-getElementsByTagName/app.js
@@ -0,0 +1,30 @@
+// Select the element or group of elements the we want
+// Decide the effect we want to apply to the selection
+
+// getElementsByTagName('tagname');
+// HTMLCollection =  array-like object
+// index, length property but not array methods (forEach)
+
+// querySelectorAll()
+// NodeList - objects are collections of nodes
+// can run forEach
+// turn them into array - spread operator [...]
+
+const headings = document.getElementsByTagName('h2');
+headings[0].style.color = 'red';
+// console.log(headings.length);
+
+const items = document.getElementsByTagName('li');
+
+// items.forEach(function(item) {
+//   console.log(item);
+// });
+items[2].style.color = 'orange';
+const betterItems = [...items];
+
+betterItems.forEach(function (item) {
+  // console.log(item);
+});
+
+console.log(items);
+console.log(betterItems);
